3654faaf45d01db2af2a0ac9a300c410aaa03e6f
Simon Schötz <simonsch.tz@gmail.com>
2024-05-30 18:57:37 +0200

    set up first api util for post request

---
+++ src/util/api/make-post.ts
@@ -0,0 +1,11 @@
+export const makePost = async <T, U>(url: string, data: T): Promise<U> => {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+
+  return response.json();
+};
