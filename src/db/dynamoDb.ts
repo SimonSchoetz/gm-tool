362bd88e92988b3b3af7aba819b2f81b82b84005
Simon Schötz <simonsch.tz@gmail.com>
2024-05-30 18:55:58 +0200

    set up dynamodb

---
+++ src/db/dynamoDb.ts
@@ -0,0 +1,15 @@
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// Load environment variables
+console.log('env:', process.env.AWS_DYNAMODB_REGION);
+//@ts-ignore
+export const dynamoDb = new DynamoDBClient({
+  region: process.env.AWS_DYNAMODB_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_DYNAMODB_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_DYNAMODB_SECRET_KEY,
+  },
+});
