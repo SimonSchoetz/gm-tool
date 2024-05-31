e5e958ceaea1d3200196e8c64a900f5882e1fb07
Simon Schötz <simonsch.tz@gmail.com>
2024-05-31 11:27:15 +0200

    adjust getUser request

--- src/db/user/getUser.ts
+++ src/db/user/getUser.ts
@@ -1,12 +1,18 @@
-import { GetItemCommand } from '@aws-sdk/client-dynamodb';
+import {
+  GetItemCommand,
+  GetItemCommandInput,
+  GetItemCommandOutput,
+} from '@aws-sdk/client-dynamodb';
 import { dynamoDb } from '../dynamoDb';
+import { DbTable } from '@/enums';
 
-export const getUser = async (email: string) => {
-  const params = {
-    TableName: 'Users',
+export const getUser = async (email: string): Promise<GetItemCommandOutput> => {
+  const params: GetItemCommandInput = {
+    TableName: DbTable.USERS,
     Key: { userId: { S: email } },
   };
 
   const command = new GetItemCommand(params);
+
   return await dynamoDb.send(command);
 };
