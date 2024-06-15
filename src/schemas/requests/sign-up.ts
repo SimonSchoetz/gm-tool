52cadb83c6856cc2800aa01ac7d987fd4e5a0de6
Simon Schötz <simonsch.tz@gmail.com>
2024-06-15 23:20:41 +0200

    fix sign up schema

--- src/schemas/requests/sign-up.ts
+++ src/schemas/requests/sign-up.ts
@@ -3,7 +3,6 @@ import { z } from 'zod';
 export const SignUpSchema = z
   .object({
     email: z.string().email(),
-    displayName: z.string().min(1, { message: 'Display Name is required' }),
     password: z
       .string()
       .min(10, { message: 'Too short, use at least 10 characters.' }),
