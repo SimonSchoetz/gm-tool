148b718694d3ab61425e24e6cc082ccdf256ee7d
Simon Schötz <simonsch.tz@gmail.com>
2024-06-16 16:52:33 +0200

    adkist create user server fn

--- src/db/user/createUser.ts
+++ src/db/user/createUser.ts
@@ -5,6 +5,7 @@ import {
   PutItemCommandInput,
   PutItemCommandOutput,
 } from '@aws-sdk/client-dynamodb';
+import { marshall } from '@aws-sdk/util-dynamodb';
 import { dynamoDb } from '../dynamoDb';
 import { DbTable } from '@/enums';
 import { SignUpData } from '@/types/requests';
@@ -19,21 +20,20 @@ export const createUser = async (
     data,
     SchemaName.SIGN_UP
   );
-
+  marshall;
   const { email, password } = validated;
-  const passwordHash = await encryptPassword(password);
 
   const params: PutItemCommandInput = {
     TableName: DbTable.USERS,
-    Item: {
-      userId: { S: generateId() },
-      createdAt: { S: new Date().toISOString() },
-      password: { S: passwordHash },
-      email: { S: email },
-    },
+    Item: marshall({
+      email,
+      userContentId: generateId(),
+      createdAt: new Date().toISOString(),
+      passwordHash: await encryptPassword(password),
+    }),
     ConditionExpression: 'attribute_not_exists(#pk)',
     ExpressionAttributeNames: {
-      '#pk': 'userId',
+      '#pk': 'email',
     },
   };
 
--- src/util/helper/generate-id/generate-id.ts
+++ src/util/helper/generate-id/generate-id.ts
@@ -1,5 +1,6 @@
 import { nanoid } from 'nanoid';
 
+// TODO: learn how to properly mock nanoid for jest testing and implement tests
 export const generateId = (): string => {
   return nanoid();
 };
