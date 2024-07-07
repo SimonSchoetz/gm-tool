4983a2818caa1be15c74b26757d782861c3285a0
Simon Schötz <simonsch.tz@gmail.com>
2024-07-07 16:57:00 +0200

    refactor submitLogin server action

--- src/actions/formSubmits/login.ts
+++ src/actions/formSubmits/login.ts
@@ -1,38 +1,37 @@
 'use server';
 
-import { getUser } from '@/db/user';
-import { LoginSchema } from '@/schemas/requests';
+import { HttpStatusCode } from '@/enums';
 import { FormSubmitResponse } from '@/types/responses';
-import { assertIsFormData } from '@/util/asserts';
-import { mapFormDataToDto } from '@/util/mapper';
-import { mapZodErrorsToErrors } from '@/util/mapper';
-import { revalidatePath } from 'next/cache';
+import { assertIsString } from '@/util/asserts';
+import { readToken } from '../token/read-token';
+import { SchemaName, parseDataWithZodSchema } from '@/schemas/util';
 import { ZodError } from 'zod';
+import { verifyUser } from '../user';
 
 export const submitLogin = async (
-  _prevState: any,
   data: unknown
 ): Promise<FormSubmitResponse> => {
   try {
-    assertIsFormData(data);
+    assertIsString(data);
 
-    const mappedData = mapFormDataToDto(data);
-    const parsed = LoginSchema.parse(mappedData);
+    const decoded = await readToken(data);
 
-    const user = await getUser(parsed.email);
+    const validatedData = parseDataWithZodSchema(decoded, SchemaName.LOGIN);
 
-    if (!user.Item) {
-      return { error: 'User not found' };
-    }
+    await verifyUser(validatedData);
 
-    console.log('TODO: Set session and reroute');
-    return { message: 'Success' };
+    console.log('TODO: Set session token and reroute');
+    return { status: HttpStatusCode.ACCEPTED };
   } catch (error) {
     if (error instanceof ZodError) {
-      return { error: mapZodErrorsToErrors(error.errors) };
+      throw new Error('Could not validate input data');
     }
 
-    throw new Error(`Unknown error during login: ${error}`);
+    if (error instanceof Error) {
+      throw new Error(error.message);
+    }
+
+    throw new Error(`Unknown error during login`); //TODO: save error log in db
   }
 
   // revalidatePath('/login'); thats just an example for when I want to show up added elements
