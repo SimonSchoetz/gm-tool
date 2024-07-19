3fdf578386473da4636c23ee87b2472c5e5f1092
Simon Schötz <simonsch.tz@gmail.com>
2024-07-19 11:42:15 +0200

    fix folder name

--- src/util/asserts/index.ts
+++ src/util/asserts/index.ts
@@ -1,3 +1,3 @@
 export * from './is-string/is-string';
-export * from './assert-form-data/assert-is-form-data';
+export * from './assert-is-form-data/assert-is-form-data';
 export * from './assert-is-zod-schema-based-type/assert-is-zod-schema-based-type';
