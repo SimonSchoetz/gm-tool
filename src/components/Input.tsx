b11022c39dd074ae8351349a6095f7aec6e3ffe9
Simon Schötz <simonsch.tz@gmail.com>
2024-07-31 17:24:23 +0200

    refactor input component and implement animated underline

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,6 +2,7 @@
 
 import { DetailedHTMLProps, InputHTMLAttributes, useState } from 'react';
 import { ConditionWrapper } from './wrapper';
+import { InputLabelUnderline } from './animations';
 
 export type InputProps = DetailedHTMLProps<
   InputHTMLAttributes<HTMLInputElement>,
@@ -14,37 +15,40 @@ export type InputProps = DetailedHTMLProps<
 const Input = ({ label, validationError, ...inputProps }: InputProps) => {
   const [focused, setFocused] = useState(false);
 
-  const labelColorFocused = validationError ? 'text-red-600' : 'text-slate-950';
-
-  const labelColorNotFocused = validationError
-    ? 'text-red-500'
-    : 'text-slate-600';
-
-  const labelColor = focused ? labelColorFocused : labelColorNotFocused;
-
-  const borderColor = validationError ? 'border-red-500' : 'border-slate-400';
+  const borderColor = validationError ? 'border-red-500' : '';
 
   const outlineColor = validationError
-    ? 'outline-red-600'
-    : 'outline-slate-950';
-
-  const errorMessageColor = focused ? 'text-red-600' : 'text-red-500';
+    ? 'outline-red-500'
+    : 'outline-gm-primary-very-high-contrast';
 
   return (
     <>
       <ConditionWrapper condition={!!label}>
-        <label className={`ml-2 ${labelColor}`}>{label}</label>
+        <div className='relative'>
+          <label
+            htmlFor={inputProps.id}
+            className={`ml-4 text-gm-primary-very-high-contrast`}
+          >
+            {label}
+
+            <InputLabelUnderline focused={focused} text={label} />
+          </label>
+        </div>
       </ConditionWrapper>
+
       <input
         {...inputProps}
-        className={`border ${borderColor} rounded-lg px-2 py-1 w-full ${outlineColor}`}
+        className={`glass-fx ${borderColor} px-4 py-2 w-full ${
+          focused ? outlineColor : ''
+        } outline-none -outline-offset-1 outline-2`}
         onFocus={() => setFocused(true)}
         onBlur={() => setFocused(false)}
         aria-invalid={!!validationError}
         aria-errormessage={validationError}
       />
+
       <ConditionWrapper condition={!!validationError}>
-        <p className={`ml-2 ${errorMessageColor}`}>{validationError}</p>
+        <p className={`ml-2 text-red-500`}>{validationError}</p>
       </ConditionWrapper>
     </>
   );
