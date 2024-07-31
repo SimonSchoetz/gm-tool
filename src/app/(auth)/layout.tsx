64432cd4f987772bbf1f95196f0c4e9b148cc792
Simon Schötz <simonsch.tz@gmail.com>
2024-07-31 17:22:57 +0200

    style adjustments to glass fx and layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,3 +1,3 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <section className='glass-fx p-8'>{children}</section>;
+  return <section className='glass-fx p-8 pb-12 m-2'>{children}</section>;
 }
