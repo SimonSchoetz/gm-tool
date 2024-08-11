07c0ddd85d21858edeb34bd8e30efb665c317e25
Simon Schötz <simonsch.tz@gmail.com>
2024-08-11 18:58:36 +0200

    set margine on max width wrapper

--- src/components/wrapper/MaxWidthWrapper.tsx
+++ src/components/wrapper/MaxWidthWrapper.tsx
@@ -1,7 +1,7 @@
 import { PropsWithChildren } from 'react';
 
 const MaxWidthWrapper = ({ children }: PropsWithChildren) => {
-  return <div className='max-w-md w-full'>{children}</div>;
+  return <div className='max-w-md w-full mx-4'>{children}</div>;
 };
 
 export default MaxWidthWrapper;
