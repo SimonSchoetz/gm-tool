58c5190c6d202a5bfd85795a3a8e3a40bd851a24
Simon Schötz <simonsch.tz@gmail.com>
2024-08-11 18:35:21 +0200

    adjust input component styling

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -38,13 +38,19 @@ const Input = ({ label, validationError, ...inputProps }: InputProps) => {
 
       <input
         {...inputProps}
-        className={`glass-fx ${borderColor} px-4 py-2 w-full ${
-          focused ? outlineColor : ''
-        } outline-none -outline-offset-1 outline-2`}
+        className={`glass-fx ${borderColor} px-4 py-2 w-full 
+        outline-none -outline-offset-1 outline-1
+        ${focused ? outlineColor : ''} 
+        disabled:opacity-20
+        disabled:cursor-not-allowed
+        hover:border-gm-primary-very-high-contrast
+        hover:border-opacity-50`}
         onFocus={() => setFocused(true)}
         onBlur={() => setFocused(false)}
         aria-invalid={!!validationError}
         aria-errormessage={validationError}
+        aria-disabled={inputProps.disabled}
+        disabled={inputProps.disabled}
       />
 
       <ConditionWrapper condition={!!validationError}>
