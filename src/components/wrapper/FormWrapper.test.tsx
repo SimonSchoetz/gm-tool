1d1ff52edb554f6128e8ef1d1522178f8ea7248c
Simon Schötz <simonsch.tz@gmail.com>
2024-08-19 16:25:31 +0200

    another form wrapper test to cover error messages

--- src/components/wrapper/FormWrapper.test.tsx
+++ src/components/wrapper/FormWrapper.test.tsx
@@ -64,12 +64,31 @@ describe('FormWrapper', () => {
     userEvent.click(screen.getByText('Submit'));
 
     await waitFor(() => {
-      // For some reason, this check has to be here within this waitFor block
-      // in order to make the test pass
-      expect(screen.getByPlaceholderText('Password')).toHaveValue('tooshort');
+      expect(screen.getByText('Submit')).toBeDisabled();
     });
+  });
+  it('should provide error messages if form is invalid', async () => {
+    const mockOnSubmit = jest.fn();
+    render(
+      <FormWrapper
+        buttonLabel='Submit'
+        schemaName={SchemaName.LOGIN}
+        submitAction={mockOnSubmit}
+      >
+        <Input id='email' placeholder='Email' value='test@example.com' />
+        <Input id='password' placeholder='Password' type='password' />
+      </FormWrapper>
+    );
+    const passwordInput = screen.getByPlaceholderText('Password');
 
-    expect(screen.getByText('Submit')).toBeDisabled();
+    userEvent.type(passwordInput, 'tooshort');
+    userEvent.click(screen.getByText('Submit'));
+
+    await waitFor(() => {
+      expect(
+        screen.getByText('Too short, use at least 10 characters.')
+      ).toBeInTheDocument();
+    });
   });
 
   it('calls onSubmit with form values when submitted', async () => {
