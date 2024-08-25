fade67fd71eb156d33b17652f4ebf262de345c0a
Simon Schötz <simonsch.tz@gmail.com>
2024-08-25 22:36:33 +0200

    adjust redirect to avoid redirection loop

--- src/util/router/redirect-to.ts
+++ src/util/router/redirect-to.ts
@@ -5,6 +5,12 @@ export const redirectTo = (
   req: NextRequest,
   route: Route
 ): NextResponse<unknown> => {
+  const { pathname } = req.nextUrl;
+
+  if (pathname === route) {
+    return NextResponse.next();
+  }
+
   const url = new URL(route, req.url);
   return NextResponse.redirect(url);
 };
