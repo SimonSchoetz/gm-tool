7336c9dff55c62727236075778a41c7c4584962e
Simon Schötz <simonsch.tz@gmail.com>
2024-08-27 18:08:28 +0200

    move squares rendering out of main component

--- src/components/animations/Backdrop.tsx
+++ src/components/animations/Backdrop.tsx
@@ -35,13 +35,61 @@ const Backdrop = () => {
     };
   }, [updateSquareMeasurements]);
 
-  const getSquares = () => {
-    const amountSquares = amountSquaresX * amountSquaresY;
-    const squares = [];
-    for (let i = 0; i < amountSquares; i++) {
+  const squares = getSquares({
+    amountSquaresX,
+    amountSquaresY,
+    squareSize,
+    maxSize,
+  });
+
+  return (
+    <div
+      style={{ width: '100vw', height: '100vh' }}
+      className='overflow-hidden absolute -z-10'
+    >
+      <div
+        style={{
+          //        square size + amount of gaps
+          top: `-${(squareSize + amountSquaresY) / 2}px`,
+          left: `-${(squareSize + amountSquaresX) / 2}px`,
+        }}
+        className='
+          bg-gm-primary-high-contrast-10 
+          w-[120vw] h-[120vh]  
+          flex flex-wrap gap-[1px]
+          absolute
+        '
+      >
+        {squares}
+      </div>
+    </div>
+  );
+};
+
+export default Backdrop;
+
+type SquaresConfig = {
+  amountSquaresX: number;
+  amountSquaresY: number;
+  squareSize: number;
+  maxSize: number;
+};
+
+const getSquares = ({
+  amountSquaresX,
+  amountSquaresY,
+  squareSize,
+  maxSize,
+}: SquaresConfig) => {
+  const squares = [];
+  for (let rowNum = 0; rowNum < amountSquaresY; rowNum++) {
+    for (let colNum = 0; colNum < amountSquaresX; colNum++) {
+      const identifier = `${rowNum}-${colNum}`;
+
       squares.push(
         <div
-          key={`${i}`}
+          id={identifier}
+          key={identifier}
           style={{
             width: `${squareSize}px`,
             height: `${squareSize}px`,
@@ -52,33 +100,6 @@ const Backdrop = () => {
         ></div>
       );
     }
-    return <>{squares}</>;
-  };
-
-  return (
-    <div
-      style={{ width: '100vw', height: '100vh' }}
-      className='overflow-hidden absolute -z-10'
-    >
-      <div
-        style={{
-          top: `-${squareSize / 2 + amountSquaresY / 2}px`,
-          left: `-${squareSize / 2 + amountSquaresX / 2}px`,
-        }}
-        className='
-        bg-gm-primary-high-contrast-10 
-        w-[120vw] 
-        h-[120vh]  
-        flex
-        flex-wrap 
-        absolute
-        gap-[1px] 
-        '
-      >
-        {getSquares()}
-      </div>
-    </div>
-  );
+  }
+  return <>{squares}</>;
 };
-
-export default Backdrop;
