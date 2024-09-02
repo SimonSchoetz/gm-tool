04f8814ec4148d052d5830df5e86b69cc82dba30
Simon Schötz <simonsch.tz@gmail.com>
2024-09-02 11:46:51 +0200

    add resend api key to parsed env

--- src/util/helper/parsed-env.ts
+++ src/util/helper/parsed-env.ts
@@ -5,6 +5,8 @@ const envSchema = z.object({
   AWS_DYNAMODB_SECRET_KEY: z.string().min(1),
   AWS_DYNAMODB_REGION: z.string().min(1),
   TOKEN_AUTH_SECRET: z.string().min(1),
+  RESEND_API_KEY: z.string().min(1),
 });
 
+//eslint-disable-next-line no-process-env
 export const parsedEnv = envSchema.parse(process.env);
