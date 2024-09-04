8538ae86c8df7ecf9fae76110eedeed8d5d9529d
Simon Schötz <simonsch.tz@gmail.com>
2024-09-04 18:18:10 +0200

    set up server action for sending emails

---
+++ src/actions/emails/send-email.ts
@@ -0,0 +1,43 @@
+'use server';
+
+import { EmailFrom } from '@/enums';
+import { assertIsString } from '@/util/asserts';
+import { parsedEnv } from '@/util/helper';
+import { ReactNode } from 'react';
+import { Resend } from 'resend';
+
+type SendEmailResponse = {
+  success: boolean;
+  createdEmailId?: string;
+  error?: string;
+};
+
+type EmailData = {
+  from: EmailFrom;
+  to: string;
+  subject: string;
+  template: ReactNode;
+};
+
+const resend = new Resend(parsedEnv.RESEND_API_KEY);
+
+export const sendEmail = async ({
+  from,
+  to,
+  subject,
+  template,
+}: EmailData): Promise<SendEmailResponse> => {
+  const { data, error } = await resend.emails.send({
+    from,
+    to,
+    subject,
+    react: template,
+  });
+
+  if (error) {
+    return { success: false, error: error.message };
+  }
+
+  assertIsString(data?.id);
+  return { success: true, createdEmailId: data.id };
+};
