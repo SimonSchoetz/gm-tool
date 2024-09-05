fc6d0a300905581c89d746931faf493c6f0148bf
Simon Schötz <simonsch.tz@gmail.com>
2024-09-05 15:03:44 +0200

    add colors to styles util

--- src/util/styles/colors.ts
+++ src/util/styles/colors.ts
@@ -1,28 +1,32 @@
 import { Colors } from '@/types/styles';
 
-export const colors: Colors = {
+export const colors = {
   dark: {
     full: '#031322',
+    '01': '#03132201',
     '10': '#0313221a',
     '30': '#0313224d',
     '50': '#03132280',
   },
   secondary: {
     full: '#2b4d57',
+    '01': '#2b4d5701',
     '10': '#2b4d571a',
     '30': '#2b4d574d',
     '50': '#2b4d5780',
   },
   accent: {
     full: '#399cbb',
+    '01': '#399cbb01',
     '10': '#399cbb1a',
     '30': '#399cbb4d',
     '50': '#399cbb80',
   },
   bright: {
     full: '#f8fbfb',
+    '01': '#f8fbfb01',
     '10': '#f8fbfb1a',
     '30': '#f8fbfb4d',
     '50': '#f8fbfb80',
   },
-};
+} satisfies Colors;
