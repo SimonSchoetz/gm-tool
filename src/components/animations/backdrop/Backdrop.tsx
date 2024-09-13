ce50bb73d2a0bc45251985543b86b027322665a9
Simon Schötz <simonsch.tz@gmail.com>
2024-09-13 11:49:50 +0200

    fade in backdrop on load

--- src/components/animations/backdrop/Backdrop.tsx
+++ src/components/animations/backdrop/Backdrop.tsx
@@ -1,14 +1,24 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { BackdropBeam, BackdropGrid } from './components';
 
 const Backdrop = () => {
   const [idList, setIdList] = useState<string[]>([]);
+  const [fadeIn, setFadeIn] = useState<boolean>(false);
+
+  useEffect(() => {
+    setFadeIn(true);
+  }, []);
 
   return (
     <div
-      style={{ width: '100vw', height: '100vh' }}
+      style={{
+        width: '100vw',
+        height: '100vh',
+        opacity: fadeIn ? 1 : 0,
+        transition: 'opacity 1s ease-in-out',
+      }}
       className='overflow-hidden absolute -z-10'
     >
       <BackdropGrid setIdList={setIdList} />
