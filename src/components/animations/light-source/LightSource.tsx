33d3d8e6aba602c93748056978be15c26d302dad
Simon Schötz <simonsch.tz@gmail.com>
2024-09-13 17:33:52 +0200

    animate border color on lights on

--- src/components/animations/light-source/LightSource.tsx
+++ src/components/animations/light-source/LightSource.tsx
@@ -1,28 +1,25 @@
 'use client';
 
 import { DivProps } from '@/app/_types';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 type LightSourceProps = {
   intensity: 'bright' | 'dim';
 } & DivProps;
 
 const LightSource = ({ intensity, ...props }: Readonly<LightSourceProps>) => {
-  const [fadeIn, setFadeIn] = useState<boolean>(false);
-
   useEffect(() => {
-    setFadeIn(true);
+    document.documentElement.style.setProperty('--light-state', 'running');
   }, []);
+
   const intensityClass =
     intensity === 'bright' ? 'light-source' : 'light-source-dim';
+  const localClassNames =
+    'light-on-animation fixed top-0 left-0 w-full h-full -z-[9]';
 
   return (
     <div
-      style={{
-        opacity: fadeIn ? 1 : 0,
-        transition: 'opacity .3s ease-in',
-      }}
-      className={`${intensityClass} fixed top-0 left-0 w-full h-full -z-[9] ${props.className}`}
+      className={`${intensityClass} ${localClassNames} ${props.className}`}
     ></div>
   );
 };
