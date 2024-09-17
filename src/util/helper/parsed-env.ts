16f3e4d878081f4a66bac40351f1d8792709f8ff
Simon Schötz <simonsch.tz@gmail.com>
2024-09-17 18:37:52 +0200

    remove aws keys

--- src/util/helper/parsed-env.ts
+++ src/util/helper/parsed-env.ts
@@ -2,9 +2,6 @@ import { z } from 'zod';
 
 const envValidator = z.object({
   HOST: z.string().min(1),
-  AWS_DYNAMODB_ACCESS_KEY: z.string().min(1), //todo: remove aws keys
-  AWS_DYNAMODB_SECRET_KEY: z.string().min(1),
-  AWS_DYNAMODB_REGION: z.string().min(1),
   TOKEN_AUTH_SECRET: z.string().min(1),
   RESEND_API_KEY: z.string().min(1),
   CONVEX_DEPLOYMENT: z.string().min(1),
