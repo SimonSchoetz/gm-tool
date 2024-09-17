2ae57b762ac414cf03b8b103b862d9955df9ec3a
Simon Schötz <simonsch.tz@gmail.com>
2024-09-17 14:09:29 +0200

    add convex keys to parsed env

--- src/util/helper/parsed-env.ts
+++ src/util/helper/parsed-env.ts
@@ -2,11 +2,13 @@ import { z } from 'zod';
 
 const envSchema = z.object({
   HOST: z.string().min(1),
-  AWS_DYNAMODB_ACCESS_KEY: z.string().min(1),
+  AWS_DYNAMODB_ACCESS_KEY: z.string().min(1), //todo: remove aws keys
   AWS_DYNAMODB_SECRET_KEY: z.string().min(1),
   AWS_DYNAMODB_REGION: z.string().min(1),
   TOKEN_AUTH_SECRET: z.string().min(1),
   RESEND_API_KEY: z.string().min(1),
+  CONVEX_DEPLOYMENT: z.string().min(1),
+  NEXT_PUBLIC_CONVEX_URL: z.string().min(1),
 });
 
 //eslint-disable-next-line no-process-env
