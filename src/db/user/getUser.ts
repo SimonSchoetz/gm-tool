95820ed8eac05aee5f3ed00d780862241a4800de
Simon Schötz <simonsch.tz@gmail.com>
2024-09-17 18:31:55 +0200

    add get user by id function

--- src/db/user/getUser.ts
+++ src/db/user/getUser.ts
@@ -5,12 +5,25 @@ import { cache } from 'react';
 import { convexDb, users } from '../convexDb';
 import { mapToAppDto } from '../util';
 import { AppUserData, DbUserData, UserDto } from '../../api/types';
-import { EmailVerificationState } from '@/enums';
+import { DbTable, EmailVerificationState } from '@/enums';
+import { Id } from '../../../convex/_generated/dataModel';
 
-export const getUserByEmail = cache(async (email: string): Promise<User> => {
-  const res = await convexDb.query(users.getUserByEmail, { email });
+export const getUserByEmail = cache(
+  async (email: User['email']): Promise<User> => {
+    const res = await convexDb.query(users.getUserByEmail, { email });
+    if (!res) {
+      throw new Error(`User with email '${email}' not found`);
+    }
+    return mapUserDto(res);
+  }
+);
+
+export const getUserById = cache(async (userId: User['id']): Promise<User> => {
+  const res = await convexDb.query(users.getUserById, {
+    userId: userId as Id<DbTable.USERS>,
+  });
   if (!res) {
-    throw new Error(`User with email '${email}' not found`);
+    throw new Error(`User with id '${userId}' not found`);
   }
   return mapUserDto(res);
 });
