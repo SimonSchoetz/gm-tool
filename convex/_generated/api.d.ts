1e7c7145cf27f2b36dea29326ae4a718bf81c513
Simon Schötz <simonsch.tz@gmail.com>
2024-09-17 14:12:44 +0200

    install convex helper and concurrently starting npm scripts

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -15,6 +15,13 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as helper_index from "../helper/index.js";
+import type * as helper_zod from "../helper/zod.js";
+import type * as tables_index from "../tables/index.js";
+import type * as tables_users_table from "../tables/users_table.js";
+import type * as users_create_users from "../users/create_users.js";
+import type * as users_get_users from "../users/get_users.js";
+import type * as users_index from "../users/index.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -24,7 +31,15 @@ import type {
  * const myFunctionReference = api.myModule.myFunction;
  * ```
  */
-declare const fullApi: ApiFromModules<{}>;
+declare const fullApi: ApiFromModules<{
+  "helper/index": typeof helper_index;
+  "helper/zod": typeof helper_zod;
+  "tables/index": typeof tables_index;
+  "tables/users_table": typeof tables_users_table;
+  "users/create_users": typeof users_create_users;
+  "users/get_users": typeof users_get_users;
+  "users/index": typeof users_index;
+}>;
 export declare const api: FilterApi<
   typeof fullApi,
   FunctionReference<any, "public">
