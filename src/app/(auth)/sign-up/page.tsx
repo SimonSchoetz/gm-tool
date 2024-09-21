e8674455c5a4882e9df5b8918b431ccbe815d39d
Simon Schötz <simonsch.tz@gmail.com>
2024-09-21 19:39:21 +0200

    marc required fields in sign up form

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -7,6 +7,8 @@ import {
 import { submitSignUp } from '@/actions/formSubmits';
 import { ValidatorName } from '@/validators/util';
 import { generateToken } from '@/actions/token';
+import AppLink, { AppLinkLayout } from '@/components/AppLink';
+import { Route } from '@/enums';
 
 export default function SignUpPage() {
   const title = 'Sign Up';
@@ -14,9 +16,17 @@ export default function SignUpPage() {
   return (
     <ContentContainer title={title}>
       <GlassPanel>
-        <p className='text-center my-5'>
-          Please fill in your data to create an account
-        </p>
+        <div className='text-center my-5'>
+          <p>Please fill in your data to create an account.</p>
+          <p>
+            Already got one?{' '}
+            <AppLink
+              title='Login &rarr;'
+              href={Route.LOGIN}
+              layout={AppLinkLayout.LINK}
+            />
+          </p>
+        </div>
 
         <FormWrapper
           schemaName={ValidatorName.SIGN_UP}
@@ -30,6 +40,7 @@ export default function SignUpPage() {
             placeholder='Email'
             label='Email'
             type='email'
+            required
           />
           <Input
             name='password'
@@ -37,6 +48,7 @@ export default function SignUpPage() {
             placeholder='Password'
             label='Password'
             type='password'
+            required
           />
           <Input
             name='confirmPassword'
@@ -44,6 +56,7 @@ export default function SignUpPage() {
             placeholder='Confirm password'
             label='Confirm password'
             type='password'
+            required
           />
         </FormWrapper>
       </GlassPanel>
