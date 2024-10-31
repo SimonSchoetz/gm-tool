3e501fb37a5cdc5e05feb33609746ab45328e218
Simon Schötz <simonsch.tz@gmail.com>
2024-10-31 11:00:39 +0100

    suppress specific sass deprecation warning

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  sassOptions: {
+    logger: {
+      warn: (message) => {
+        if (
+          message.includes(
+            'Deprecation The legacy JS API is deprecated and will be removed in Dart Sass 2.0.0.'
+          )
+        )
+          return;
+        console.warn(message);
+      },
+    },
+  },
+};
 
 export default nextConfig;
