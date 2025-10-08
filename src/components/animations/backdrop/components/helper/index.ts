3c01b7b613ba262cfb83ad508f70be5f7b433bfc
Simon Schötz <simonsch.tz@gmail.com>
2025-10-08 18:14:06 +0200

    refactor backdrop logic to canvas

--- src/components/animations/backdrop/components/helper/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './get-path-square-ids';
-export * from './decide-direction';
-export * from './get-square-positions';
-export * from './map-path-coordinates';
-export * from './get-id-list-without-extra-rows';
--- src/components/animations/backdrop/components/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as BackdropBeam } from './BackdropBeam';
-export { default as BackdropGrid } from './BackdropGrid';
