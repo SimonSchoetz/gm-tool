04628ed6a9bc2e6d85bac5330835c6be9fbd64c9
Simon Schötz <simonsch.tz@gmail.com>
2025-11-30 13:37:47 +0100

    make border radius of glass panel changeable

--- app/src/components/GlassPanel/GlassPanel.tsx
+++ app/src/components/GlassPanel/GlassPanel.tsx
@@ -3,18 +3,30 @@ import { HtmlProps } from '@/types';
 import { cn } from '@/util';
 import './GlassPanel.css';
 
+type RadiusSize = 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl' | 'full' | 'none';
+
 type GlassPanelProps = {
-  intensity?: 'dim' | 'bright';
+  intensity?: React.ComponentProps<typeof LightSource>['intensity'];
+  radius?: RadiusSize;
 } & HtmlProps<'div'>;
 
 const GlassPanel = ({
   className = '',
   children,
   intensity = 'dim',
+  radius = '2xl',
+  style,
   ...props
 }: GlassPanelProps) => {
   return (
-    <div className={cn('glass-panel', 'glass-fx', className)} {...props}>
+    <div
+      className={cn('glass-panel', 'glass-fx', className)}
+      style={{
+        ...style,
+        borderRadius: `var(--radius-${radius})`,
+      }}
+      {...props}
+    >
       <LightSource intensity={intensity} />
       {children}
     </div>
