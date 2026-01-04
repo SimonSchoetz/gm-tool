e55c355bae0da5d82c7ac62f6ec5fe1ccb598d40
Simon Schötz <simonsch.tz@gmail.com>
2026-01-05 00:25:36 +0100

    vibe code first nav functionality

--- app/src/components/SideBarNav/SideBarNav.tsx
+++ app/src/components/SideBarNav/SideBarNav.tsx
@@ -2,22 +2,52 @@ import { FCProps, HtmlProps } from '@/types';
 import './SideBarNav.css';
 import GlassPanel from '../GlassPanel/GlassPanel';
 import ActionContainer from '../ActionContainer/ActionContainer';
+import { useRouter } from '@tanstack/react-router';
+import { cn } from '@/util';
 
 type Props = HtmlProps<'aside'>;
 
 export const SideBarNav: FCProps<Props> = ({ ...props }) => {
+  const router = useRouter();
+  const canGoBack = router.history.location.state?.key !== undefined;
+  const canGoForward = window.history.length > 1;
+
+  const handleBack = () => {
+    if (canGoBack) {
+      router.history.back();
+    }
+  };
+
+  const handleForward = () => {
+    if (canGoForward) {
+      router.history.forward();
+    }
+  };
+
   return (
     <aside {...props}>
       <GlassPanel>
         <div className='fw-bw-btn-container'>
-          <ActionContainer onClick={() => 'Go Back'}>
-            <GlassPanel className='fw-bw-btn content-center'>
+          <ActionContainer onClick={handleBack}>
+            <GlassPanel
+              className={cn(
+                'fw-bw-btn',
+                'content-center',
+                !canGoBack && 'disabled'
+              )}
+            >
               <span> &larr; </span>
             </GlassPanel>
           </ActionContainer>
 
-          <ActionContainer onClick={() => 'Go Forward'}>
-            <GlassPanel className='fw-bw-btn content-center'>
+          <ActionContainer onClick={handleForward}>
+            <GlassPanel
+              className={cn(
+                'fw-bw-btn',
+                'content-center',
+                !canGoForward && 'disabled'
+              )}
+            >
               <span> &rarr; </span>
             </GlassPanel>
           </ActionContainer>
