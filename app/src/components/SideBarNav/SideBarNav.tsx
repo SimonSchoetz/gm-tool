e0f7d6122656d248da2572485521961b20e48225
Simon Schötz <simonsch.tz@gmail.com>
2026-01-05 00:20:43 +0100

    outline nav bar basic navigation button

--- app/src/components/SideBarNav/SideBarNav.tsx
+++ app/src/components/SideBarNav/SideBarNav.tsx
@@ -1,6 +1,7 @@
 import { FCProps, HtmlProps } from '@/types';
 import './SideBarNav.css';
 import GlassPanel from '../GlassPanel/GlassPanel';
+import ActionContainer from '../ActionContainer/ActionContainer';
 
 type Props = HtmlProps<'aside'>;
 
@@ -8,7 +9,19 @@ export const SideBarNav: FCProps<Props> = ({ ...props }) => {
   return (
     <aside {...props}>
       <GlassPanel>
-        <h1>Side Bar Nav</h1>
+        <div className='fw-bw-btn-container'>
+          <ActionContainer onClick={() => 'Go Back'}>
+            <GlassPanel className='fw-bw-btn content-center'>
+              <span> &larr; </span>
+            </GlassPanel>
+          </ActionContainer>
+
+          <ActionContainer onClick={() => 'Go Forward'}>
+            <GlassPanel className='fw-bw-btn content-center'>
+              <span> &rarr; </span>
+            </GlassPanel>
+          </ActionContainer>
+        </div>
       </GlassPanel>
     </aside>
   );
