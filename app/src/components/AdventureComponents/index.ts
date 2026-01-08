469215c5d2611727c7f0bf3f47c326cb16567e5c
Simon Schötz <simonsch.tz@gmail.com>
2026-01-08 17:11:43 +0100

    move parts of adventure form to adventure screen

--- app/src/components/AdventureComponents/index.ts
+++ app/src/components/AdventureComponents/index.ts
@@ -2,3 +2,4 @@ export * from './NewAdventureBtn/NewAdventureBtn';
 export * from './ToAdventureBtn/ToAdventureBtn';
 export { default as AdventureFrame } from './AdventureFrame/AdventureFrame';
 export { default as CreateAdventurePopUp } from './CreateAdventurePopUp/CreateAdventurePopUp';
+export * from './UploadAdventureImgBtn/UploadAdventureImgBtn';
--- app/src/screens/adventure/AdventureScreen.tsx
+++ app/src/screens/adventure/AdventureScreen.tsx
@@ -1,14 +1,65 @@
 import { FCProps } from '@/types';
 import './AdventureScreen.css';
-import { GlassPanel } from '@/components';
+import { GlassPanel, Input, Textarea } from '@/components';
 import { cn } from '@/util';
+import { useParams } from '@tanstack/react-router';
+import { UpdateAdventureFormData, useAdventures } from '@/data/adventures';
+import { UploadAdventureImgBtn } from '@/components/AdventureComponents';
+import { FormEvent, useState } from 'react';
 
 type Props = object;
 
 export const AdventureScreen: FCProps<Props> = ({ ...props }) => {
+  const { adventureId } = useParams({ from: '/adventures/$adventureId' });
+
+  const { getAdventure } = useAdventures();
+  const adventure = getAdventure(adventureId);
+
+  const { updateAdventure } = useAdventures();
+  const [formData, setFormData] = useState<UpdateAdventureFormData>({
+    title: adventure.title ?? '',
+    description: adventure.description ?? '',
+    imgFilePath: adventure.image_id ?? '',
+  });
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    try {
+      await updateAdventure(adventureId, formData);
+      setFormData({ title: '', description: '', imgFilePath: '' });
+      // onSuccess();
+    } catch (error) {
+      console.error('Failed to create adventure:', error);
+    }
+  };
+
+  const updateFormData = (value: Partial<UpdateAdventureFormData>) => {
+    setFormData({ ...formData, ...value });
+  };
+
   return (
     <GlassPanel className={cn('adventure-screen')} {...props}>
-      AdventureScreen
+      <form onSubmit={handleSubmit}>
+        <div>
+          <Input
+            type='text'
+            placeholder='Adventure Title *'
+            value={formData.title}
+            onChange={(e) => updateFormData({ title: e.target.value })}
+            required
+          />
+          <Textarea
+            placeholder='Description'
+            value={formData.description}
+            onChange={(e) => updateFormData({ description: e.target.value })}
+            rows={4}
+          />
+        </div>
+
+        <UploadAdventureImgBtn
+          onClick={(filePath) => updateFormData({ imgFilePath: filePath })}
+        />
+      </form>
     </GlassPanel>
   );
 };
