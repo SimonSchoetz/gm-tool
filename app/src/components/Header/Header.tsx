47bad3b0e0abced146476fe5a1337cd6e938a3fb
Simon Schötz <simonsch.tz@gmail.com>
2026-01-09 14:34:13 +0100

    adjust header title

--- app/src/components/Header/Header.tsx
+++ app/src/components/Header/Header.tsx
@@ -3,16 +3,29 @@ import './Header.css';
 import GlassPanel from '../GlassPanel/GlassPanel';
 import { cn } from '@/util';
 import { useAdventures } from '@/data/adventures';
+import { useRouterState } from '@tanstack/react-router';
+import { Routes } from '@/routes';
 
 type Props = object;
 
 export const Header: FCProps<Props> = ({ ...props }) => {
-  const { adventure } = useAdventures();
+  const router = useRouterState();
+
+  const getHeading = (): string => {
+    if (router.location.href === Routes.ADVENTURES) {
+      return 'Adventures';
+    }
+    if (router.location.href.includes(Routes.ADVENTURE)) {
+      const { adventure } = useAdventures();
+      return `${adventure?.title}`;
+    }
+    return 'GM Tool';
+  };
 
   return (
     <header>
       <GlassPanel className={cn('header-content')} {...props}>
-        {adventure ? adventure.title : 'GM Tool'}
+        <h1>{getHeading()}</h1>
       </GlassPanel>
     </header>
   );
