0c2ff56d6d6a637828a436ab970ce687502b609c
Simon Schötz <info@simonschoetz.de>
2026-05-12 20:25:48 +0200

    add custom icon to date input for maximum control

--- app/src/components/DateInput/DateInput.tsx
+++ app/src/components/DateInput/DateInput.tsx
@@ -1,11 +1,25 @@
+import { useRef } from 'react';
+import { CalendarIcon } from 'lucide-react';
 import { cn } from '@/util';
 import { FCProps, HtmlProps } from '@/types';
+import { ClickableIcon } from '../ClickableIcon';
 import './DateInput.css';
 
 type DateInputProps = Omit<HtmlProps<'input'>, 'type'>;
 
 export const DateInput: FCProps<DateInputProps> = ({ className, ...props }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <input type='date' className={cn('date-input', className)} {...props} />
+    <div className={cn('date-input-wrapper', className)}>
+      <input ref={inputRef} type='date' className='date-input' {...props} />
+      <ClickableIcon
+
+        icon={<CalendarIcon className='date-input-icon' />}
+        label='Open date picker'
+        title='Open date picker'
+        onClick={() => { inputRef.current?.showPicker(); }}
+      />
+    </div>
   );
 };
