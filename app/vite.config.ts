b78cd31a5718331d41a171ea43fa92d36520b2d1
Simon Schötz <info@simonschoetz.de>
2026-06-02 17:58:03 +0200

    adjust chunk size for frontend builds

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -30,6 +30,7 @@ export default defineConfig(async () => ({
 
   build: {
     target: 'esnext',
+    chunkSizeWarningLimit: 1000,
   },
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
