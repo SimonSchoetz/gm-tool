7c5c63baa6f48c1ea3c9871bd0024ae65bfda974
Simon Schötz <info@simonschoetz.de>
2026-06-28 14:37:45 +0200

    refactor color input

--- app/src/components/ColorInput/ColorInput.tsx
+++ app/src/components/ColorInput/ColorInput.tsx
@@ -3,9 +3,13 @@ import { FCProps } from '@/types';
 import { hexToRgb, rgbToHex } from './helper';
 import './ColorInput.css';
 
-type Props = { value: string; onChange: (value: string) => void };
+type Props = {
+  label: string;
+  value: string;
+  onChange: (value: string) => void;
+};
 
-export const ColorInput: FCProps<Props> = ({ value, onChange }) => {
+export const ColorInput: FCProps<Props> = ({ label, value, onChange }) => {
   const inputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
@@ -13,7 +17,10 @@ export const ColorInput: FCProps<Props> = ({ value, onChange }) => {
   }, []);
 
   return (
-    <label className='color-input-wrapper'>
+    <label
+      className='color-input-wrapper'
+      style={{ '--rt-color-input-color': value } as React.CSSProperties}
+    >
       <input
         ref={inputRef}
         type='color'
@@ -23,10 +30,7 @@ export const ColorInput: FCProps<Props> = ({ value, onChange }) => {
           onChange(hexToRgb(e.target.value));
         }}
       />
-      <span
-        className='color-input-dot'
-        style={{ '--rt-color-input-color': value } as React.CSSProperties}
-      />
+      <span className='color-input-label'>{label}</span>
     </label>
   );
 };
