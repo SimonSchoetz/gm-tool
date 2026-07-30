c40d3b1f53356893c5a6e9ad46ff15238ea82759
Simon Schötz <info@simonschoetz.de>
2026-07-30 13:40:22 +0200

    feat(item-duplication): fix return in helper fn to make all tests pass

--- app/src/components/TextEditor/components/FloatingToolbar/components/LinkRow/helper/getSelectionLinkUrl.ts
+++ app/src/components/TextEditor/components/FloatingToolbar/components/LinkRow/helper/getSelectionLinkUrl.ts
@@ -3,7 +3,7 @@ import { $isLinkNode } from '@lexical/link';
 
 export const getSelectionLinkUrl = (): string | null => {
   const selection = $getSelection();
-  if (!$isRangeSelection(selection)) return;
+  if (!$isRangeSelection(selection)) return null;
   const nodes = selection.getNodes();
   for (const node of nodes) {
     if ($isLinkNode(node)) return node.getURL();
